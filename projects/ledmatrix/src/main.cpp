16082483b55fc932f7c05cd18337a5c37e545a8d
Bino Maiheu <bino.maiheu@vito.be>
2019-03-23 09:46:22 +0100

    added ledmatrix project for ccl

---
+++ projects/ledmatrix/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <LedMatrix.h>
+
+
+#define NUMBER_OF_DEVICES 1 //number of led matrix connect in series
+
+#define CS_PIN 15
+#define CLK_PIN 14
+#define MISO_PIN 2 //we do not use this pin just fill to match constructor
+#define MOSI_PIN 12 // Master Out Slave In --> connect to DIN of led matrix
+
+LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+  
+boolean face[8][8] = {
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,1,0},
+    {0,0,0,1,1,0,0,0},
+    {0,0,0,1,1,0,0,0},
+    {1,0,0,0,0,0,0,1},
+    {0,1,1,1,1,1,1,0},
+    {0,0,0,1,1,0,0,0}
+  };
+
+void setup() {
+  ledMatrix.init();
+}
+
+void loop() {
+  ledMatrix.clear();
+  ledMatrix.commit();
+  delay(250);
+  ledMatrix.tekenMatrix(face);
+  ledMatrix.commit();
+  delay(250);
+}
